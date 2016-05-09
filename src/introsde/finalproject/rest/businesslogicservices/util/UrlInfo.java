b2e2362ecd8ee8289f98fe7aeb93eff27faf2b68
yuly-sanchez <yuly.sanchez29@gmail.com>
2016-05-09 11:50:18 +0200

    update services's heroku url

--- src/introsde/finalproject/rest/businesslogicservices/util/UrlInfo.java
+++ src/introsde/finalproject/rest/businesslogicservices/util/UrlInfo.java
@@ -4,9 +4,10 @@ public class UrlInfo {
 	
 	public UrlInfo() {}
 	
-	static final String businessLogicUrl = "http://127.0.1.1:5700/sdelab/businessLogic-service";
-	static final String storageUrl = "http://127.0.1.1:5701/sdelab/storage-service";
-	
+	//static final String businessLogicUrl = "http://127.0.1.1:5700/sdelab/businessLogic-service";
+	static final String businessLogicUrl = " https://fierce-sea-36005.herokuapp.com/sdelab/businessLogic-service";
+	//static final String storageUrl = "http://127.0.1.1:5701/sdelab/storage-service";
+	static final String storageUrl = "https://warm-hamlet-95336.herokuapp.com/sdelab/storage-service";
 	/**
 	 * This method is used to get the business logic url
 	 * @return
